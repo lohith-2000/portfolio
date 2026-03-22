472569a20288c90d17b8e1854ae4639f0ba1cb34
lohith-2000 <lohithkongara123@gmail.com>
2026-03-22 17:31:12 +0530

    first commit

---
+++ code/sc.js
@@ -0,0 +1,10 @@
+function func1(){
+    document.writeln("hi,iam function1<br>");
+}
+function func2(){
+    document.writeln("hi,iam function2<br>");
+}
+function func3(){
+    document.writeln("hi,iam function2<br>");
+}
+func1();
